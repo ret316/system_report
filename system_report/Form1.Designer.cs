e9cf9e3ef802842d98c119bdd61a30d1758d6414
ret316 <ret316@yandex.ru>
2017-06-08 11:05:30 +0400

    first

---
+++ system_report/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace system_report
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOpenForlder = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btOpenFile = new System.Windows.Forms.Button();
+            this.btDeleteFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btOpenForlder
+            // 
+            this.btOpenForlder.Location = new System.Drawing.Point(12, 37);
+            this.btOpenForlder.Name = "btOpenForlder";
+            this.btOpenForlder.Size = new System.Drawing.Size(65, 39);
+            this.btOpenForlder.TabIndex = 0;
+            this.btOpenForlder.Text = "Выбрать папку";
+            this.btOpenForlder.UseVisualStyleBackColor = true;
+            this.btOpenForlder.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(154, 129);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(74, 28);
+            this.btSave.TabIndex = 1;
+            this.btSave.Text = "Сохранить";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(299, 26);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(309, 409);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // btOpenFile
+            // 
+            this.btOpenFile.Location = new System.Drawing.Point(83, 37);
+            this.btOpenFile.Name = "btOpenFile";
+            this.btOpenFile.Size = new System.Drawing.Size(65, 39);
+            this.btOpenFile.TabIndex = 3;
+            this.btOpenFile.Text = "Добавить файл";
+            this.btOpenFile.UseVisualStyleBackColor = true;
+            this.btOpenFile.Click += new System.EventHandler(this.btOpenFile_Click);
+            // 
+            // btDeleteFile
+            // 
+            this.btDeleteFile.Location = new System.Drawing.Point(154, 37);
+            this.btDeleteFile.Name = "btDeleteFile";
+            this.btDeleteFile.Size = new System.Drawing.Size(65, 39);
+            this.btDeleteFile.TabIndex = 4;
+            this.btDeleteFile.Text = "Удалить файл";
+            this.btDeleteFile.UseVisualStyleBackColor = true;
+            this.btDeleteFile.Click += new System.EventHandler(this.btDeleteFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 453);
+            this.Controls.Add(this.btDeleteFile);
+            this.Controls.Add(this.btOpenFile);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.btOpenForlder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOpenForlder;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btOpenFile;
+        private System.Windows.Forms.Button btDeleteFile;
+    }
+}
+
---
+++ system_report/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace system_report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*private void btOpen_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.Filter = "doc files (*.doc)|*.doc|docx files (*.docx)|*.docx|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }*/
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show(FBD.SelectedPath);
+                string[] docx = GetFileNames(FBD.SelectedPath, "*.docx");
+
+                for (int i = 0; i < docx.Length; i++)
+                {
+                    checkedListBox1.Items.Add(docx[i]);
+                }
+            }
+        }
+
+        private static string[] GetFileNames(string path, string filter)
+        {
+            string[] files = Directory.GetFiles(path, filter);
+            for (int i = 0; i < files.Length; i++)
+                files[i] = Path.GetFileName(files[i]);
+            return files;
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "doc files (*.doc)|*.doc|docx files (*.docx)|*.docx|All files (*.*)|*.*"; ;
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void readinfo(string filename)
+        {
+
+        }
+
+        private void writeinfo(string filename)
+        {
+
+        }
+
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+            {
+                Stream myStream = null;
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                openFileDialog1.InitialDirectory = "c:\\";
+                //openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog1.Filter = "doc files (*.doc)|*.doc|docx files (*.docx)|*.docx|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        if ((myStream = openFileDialog1.OpenFile()) != null)
+                        {
+                            using (myStream)
+                            {
+                                checkedListBox1.Items.Add(openFileDialog1.SafeFileName);
+                                // Insert code to read the stream here.
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btDeleteFile_Click(object sender, EventArgs e)
+        {
+            for (int i = checkedListBox1.Items.Count - 1; i >= 0; i--)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                    checkedListBox1.Items.RemoveAt(i);
+            }
+        }
+    }
+}
